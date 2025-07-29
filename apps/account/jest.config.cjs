a5962bbf731686ce6b254ef5dbddb5a1f4b2c454
Michal Jach <michaljach@gmail.com>
2025-07-29 18:22:42 +0200

    test runner fixes

--- apps/account/jest.config.cjs
+++ apps/account/jest.config.cjs
@@ -11,20 +11,7 @@ const customJestConfig = {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
   testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**/*.{js,jsx,ts,tsx}',
-    '!src/**/*.d.ts',
-    '!src/**/*.stories.{js,jsx,ts,tsx}',
-    '!src/**/*.test.{js,jsx,ts,tsx}',
-    '!src/**/*.spec.{js,jsx,ts,tsx}',
-    '!src/**/index.{js,jsx,ts,tsx}',
-    '!src/**/__tests__/**',
-    '!src/**/__mocks__/**'
-  ],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'html']
+  testPathIgnorePatterns: ['/node_modules/', '/.next/']
 }
 
 module.exports = createJestConfig(customJestConfig)
